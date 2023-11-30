ba7c6f06c361033c69ea4261497b7beba35f1587
NarHakobyann <narhakobyann00@gmail.com>
2023-11-30 14:05:34 +0400

    create my second branch

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
         {/* Narek jan ynduni */}
         Narek`s Branch
       </h1>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, natus soluta! Perferendis eveniet ipsa architecto impedit voluptates fuga laudantium, necessitatibus perspiciatis facere neque reprehenderit quasi modi inventore obcaecati vel doloremque.
+      </h2>
     </div>
   )
 }
