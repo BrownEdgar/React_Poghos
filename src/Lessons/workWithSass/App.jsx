f134b69035af84509e43229459ed6fb50e4cfa68
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-30 14:42:43 +0400

    added sass example

---
+++ src/Lessons/workWithSass/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss'
+
+export default function App() {
+
+  return (
+    <div className='App'>
+      <h1 className='App__title'>
+        Lorem ipsum dolor sit.
+        <span>(3)</span>
+      </h1>
+      <p className='App__desc App__desc-black'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat odit consequuntur pariatur placeat. Accusantium quidem voluptatum reiciendis nihil, laboriosam explicabo optio recusandae quasi quisquam quae veniam, deleniti amet laudantium odio?
+      </p>
+      <p className='App__desc App__desc-blue'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat odit consequuntur pariatur placeat. Accusantium quidem voluptatum reiciendis nihil, laboriosam explicabo optio recusandae quasi quisquam quae veniam, deleniti amet laudantium odio?
+      </p>
+    </div>
+  )
+}
+
