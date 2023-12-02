b6c31c6763fa4c9da77607b076aa10c18cd6262e
Aghasi <aghasi.sargsyan.94@gmail.com>
2023-12-02 14:52:22 +0400

    React Lesson 1

--- src/Lessons/PropTypes/App.jsx
+++ src/Lessons/PropTypes/App.jsx
@@ -7,7 +7,7 @@ import {useState} from 'react'
 const ImageUrl = 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 export default function App() {
-    const [stories, setstories] = useState([
+    const [stories, setStories] = useState([
         {id: 1, title: 'Private Villa', image: ImageUrl, description: 'Lorem ipsum dolor sit amet consectetur adipisicing eli. Commodi, optio.'},
         {id: 2, title: 'Private Villa', image: ImageUrl, description: 'Lorem ipsum dolor sit amet consectetur adipisicing eli. Commodi, optio.'},
         {id: 3, title: 'Private Villa', image: ImageUrl, description: 'Lorem ipsum dolor sit amet consectetur adipisicing eli. Commodi, optio.'},
