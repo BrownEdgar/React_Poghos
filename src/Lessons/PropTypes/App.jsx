bf5018234b76e7d707fb7092412310b8093ee612
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-12-02 14:50:25 +0400

    propstypes example added

---
+++ src/Lessons/PropTypes/App.jsx
@@ -0,0 +1,27 @@
+import Child from './Child'
+import { useState } from 'react'
+
+import './App.scss'
+
+const ImageUrl = 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+
+export default function App() {
+  const [stories, setStories] = useState([
+    { id: 1, title: 'Private Villa', image: ImageUrl, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, optio.' },
+    { id: 2, title: 'Private Villa', image: ImageUrl, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, optio.' },
+    { id: 3, title: 'Private Villa', image: ImageUrl, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, optio.' },
+  ])
+  return (
+    <div className='App'>
+      <h1 className='App__Title'>Our Story</h1>
+      <p className='App__Desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem aut consequatur praesentium accusamus cumque illum inventore eum mollitia repudiandae porro. Accusamus fuga, reiciendis nihil iste explicabo autem ex repellendus consectetur?</p>
+      <div className='App__List'>
+        {
+          stories.map(elem => {
+            return <Child key={elem.id} elem={elem} />
+          })
+        }
+      </div>
+    </div>
+  )
+}
---
+++ src/Lessons/PropTypes/Child.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+export default function Child({ elem }) {
+  return (
+    <div className="App__List__item">
+      <img src={elem.image} />
+      <div className='content'>
+        <h2>{elem.title}</h2>
+        <p>{elem.description}</p>
+        <button>Go somewhere</button>
+      </div>
+    </div>
+  )
+}
+
+
+
+Child.propTypes = {
+  elem: PropTypes.exact({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+  })
+}
+
