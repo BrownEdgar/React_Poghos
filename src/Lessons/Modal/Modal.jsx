3c6d321a7a815699db7cdfa036fb3c5bb34a3171
Aghasi <aghasi.sargsyan.94@gmail.com>
2023-12-05 14:42:24 +0400

    Modal lesson

--- src/Lessons/Modal/Modal.jsx
+++ src/Lessons/Modal/Modal.jsx
@@ -1,11 +1,23 @@
 import './Modal.scss'
+import PropTypes from 'prop-types';
 
-export default function Modal() {
+
+export default function Modal({ toggleModal , removeItem}) {
   return (
     <div className='Modal'>
         <div className='Modal__content'>
-            <h1>Lorem ipsum dolor sit.</h1>
+            <h1 className='Modal__title'>Delete this story?</h1>
+            <button onClick={() => {
+                toggleModal();
+                removeItem();
+            }}>Delete</button>
+            <button onClick={toggleModal}>Cancel</button>
         </div>
     </div>
   )
+}
+
+Modal.propTypes = {
+    toggleModal: PropTypes.func.isRequired,
+    removeItem: PropTypes.func.isRequired,
 }
