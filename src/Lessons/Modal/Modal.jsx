6665f3551866c7ca13d2edbd112ee1c5d7856da9
Aghasi <aghasi.sargsyan.94@gmail.com>
2023-12-05 14:03:26 +0400

    Modal

---
+++ src/Lessons/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import './Modal.scss'
+
+export default function Modal() {
+  return (
+    <div className='Modal'>
+        <div className='Modal__content'>
+            <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+    </div>
+  )
+}
