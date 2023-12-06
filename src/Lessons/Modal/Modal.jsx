d8487542728d03f803e1fa21082c678449d2156d
EdKarapetian <edo.karapetian@gmail.com>
2023-12-06 23:24:42 +0400

    Added Cities component

--- src/Lessons/Modal/Modal.jsx
+++ src/Lessons/Modal/Modal.jsx
@@ -6,7 +6,7 @@ export default function Modal({ toggleModal, removeItem }) {
   return (
     <div className='Modal'>
       <div className='Modal__content'>
-        <h1 className='Modal__title'>Delete this story?</h1>
+        <h1 className='Modal__title'>Delete this city?</h1>
         <button onClick={toggleModal}>Cancel</button>
         <button onClick={() => {
           removeItem();
