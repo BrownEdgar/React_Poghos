04583dd58564c65c6f7389fc549a0a2d62901acc
EdKarapetian <edo.karapetian@gmail.com>
2023-12-10 13:38:05 +0400

    Added deletable movies homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './Lessons/Homework/App'
+import App from './Lessons/Homework/DeletableMovies/Movies'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
