f7865124d31e6c6aa7f98a163f0e308c3acec50b
EdKarapetian <edo.karapetian@gmail.com>
2023-12-13 14:36:16 +0400

    Added Axios quotes and carts homework

--- src/Lessons/Homework/DeletableUsers/App.jsx
+++ src/Lessons/Homework/DeletableUsers/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import Child from './Child'
-import Modal from '../Modal/Modal'
+import Modal from '../../Modal/Modal'
+
 
 
 const DB_URL = import.meta.env.VITE_DB_URL
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './Lessons/axiosExample/App'
+import App from './Lessons/Homework/axios/Products'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
