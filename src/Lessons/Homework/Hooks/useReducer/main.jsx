9814ff1ef43fac38b73b135c27b567106511f832
Vahan <vahanmatosyan@mail.ru>
2023-12-16 11:25:27 +0300

    Homework useReducer

---
+++ src/Lessons/Homework/Hooks/useReducer/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './Lessons/hooks/useReducer/App'
+import App from './Lessons/Homework/Hooks/useReducer/App'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
