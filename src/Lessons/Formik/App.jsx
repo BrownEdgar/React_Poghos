4f5bda32aa36e0c290c6defd1917d9a20eea0e2e
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-12-23 14:49:51 +0400

    filter example

--- src/Lessons/Formik/App.jsx
+++ src/Lessons/Formik/App.jsx
@@ -1,30 +1,29 @@
-import React, { useState } from 'react'
-import { ErrorMessage, Field, Form, Formik } from 'formik'
-
-import { object as YupObject, string } from 'yup'
-
 import './App.scss'
+import { useState } from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { object as YupObject, string } from 'yup'
 import { nanoid } from 'nanoid'
 import List from './List/List'
-
+import jsonData from './data.json'
 const initialValues = {
   username: '',
   email: '',
-  lamguages: ''
+  language: ''
 }
-const languagesList = ['html', 'css', 'js', 'react', 'next']
+const languagesList = ['html', 'css', 'javascript', 'react', 'next', "all"]
 
 const validationSchema = YupObject({
   username: string().max(18).min(3).matches(/^[A-Z]/, 'Must by start with uppercase').required(),
   email: string().email().required(),
-  languages: string().oneOf(languagesList)
+  language: string().oneOf(languagesList)
 
 })
 
 export default function App() {
-  const [users, setusers] = useState([])
+  const [users, setusers] = useState([]);
 
   const handleSubmit = (values, { resetForm }) => {
+    console.log('values', values)
     const user = {
       id: nanoid(),
       ...values
@@ -59,10 +58,10 @@ export default function App() {
                 </ErrorMessage>
                 <Field type="text" name='username' />
                 <Field type="email" name='email' />
-                <Field as='select' name="languages">
+                <Field as='select' name="language" defaultvalue="" required>
                   <option value="html">html</option>
                   <option value="css">css</option>
-                  <option value="js">js</option>
+                  <option value="javascript">javascript</option>
                   <option value="react">react</option>
                   <option value="next">next.js</option>
                 </Field>
@@ -74,7 +73,7 @@ export default function App() {
         }
       </Formik>
       <hr />
-      <List languagesList={languagesList} />
+      <List languagesList={languagesList} users={users} />
     </div>
   )
 }
--- src/Lessons/Formik/List/List.jsx
+++ src/Lessons/Formik/List/List.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import classNames from 'classnames';
 
 import './List.scss'
 
-export default function List({ languagesList }) {
+export default function List({ languagesList, users }) {
+  const [currentLanguage, setCurrentLanguage] = useState('all');
+  const [options, setOptions] = useState({})
+
+  const setLanguage = (n) => {
+    setCurrentLanguage(n)
+  }
+
+  useEffect(() => {
+    const newOptions = users.reduce((acc, nextUser) => {
+      acc[nextUser?.language] ? acc[nextUser?.language] += 1 : acc[nextUser.language] = 1
+      return acc;
+    }, {});
+    setOptions({ ...newOptions, all: users.length })
+  }, [users.length])
+
   return (
     <div className='List'>
       <div className="List__buttons">
         {languagesList.map(elem => {
-          return <button key={elem}>{elem}</button>
+          return <button
+            key={elem}
+            onClick={() => setLanguage(elem)}
+            className={classNames({
+              active: currentLanguage === elem
+            })}
+          >
+            {elem} ({options[elem]})
+          </button>
         })}
+      </div>
+      <div className='List__users'>
+        {
+          users.map(user => {
+            if (currentLanguage === user.language || currentLanguage === 'all') {
+              return (
+                <div key={user.id}>
+                  <h2>{user.username}</h2>
+                  <p>language: <span>{user.language}</span></p>
+                </div>
+              )
+            }
+
+          })
+        }
       </div>
     </div>
   )
