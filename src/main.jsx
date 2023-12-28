5953277932b0ea492aedad40941e0b5ed2191a54
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-12-28 14:38:49 +0400

    todo list with localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './Lessons/hooks/useMemo/App'
+import App from './Lessons/localStorage/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
